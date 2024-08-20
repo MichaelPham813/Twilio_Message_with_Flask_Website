d35e2952985445c5add16339b0c103094b55c54a
Duc Pham <72677612+MichaelPham813@users.noreply.github.com>
2024-08-20 15:05:10 +0000

    Put phone number in environment variable for privacy

--- twilio_message.py
+++ twilio_message.py
@@ -8,11 +8,11 @@ import os
 #Variable that have been embedded into the environment for secret
 account_sid = os.environ['SID_CODE']
 account_auth = os.environ['ACCOUNT_TOKEN']
-account_phone_number = "+14153673858"
+account_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
 ##########################################################
 
 #My number
-target_phone_number = "+17059885602"
+target_phone_number = os.getenv("OWNER_PHONE_NUMBER")
 
 #Messages
 msg = f"Hello you have been selected to attend CIA secret program, {characters_HP.character_selection()}, they will be your mentor, {weather.weather()} is your location and temperature, {total_people.total_people_space()} number of people in space"
